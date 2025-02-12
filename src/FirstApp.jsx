a18d74a683df6d7efc30057c8a6a9cec6886f47d
king-beard <chucho_herrera10@icloud.com>
2025-02-11 22:12:03 -0600

    print variable

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -3,7 +3,7 @@ import { Fragment } from "react"
 export const FirstApp = () => {
   return (
     <Fragment>
-      <h1>Kakaroto!!!!</h1>
+      <h1>Jesus</h1>
       <p>Kakaroto!!!!</p>
     </Fragment>
   )
