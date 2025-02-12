79c9ea4b24a00ae933487e545a39f1b07c03cbff
king-beard <chucho_herrera10@icloud.com>
2025-02-12 01:17:33 -0600

    handleSubtract and handleReset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -6,11 +6,15 @@ export const CounterApp = ({ value }) => {
   const [counter, setCounter] = useState(value)
 
   const handleAdd = () => setCounter(counter + 1)
+  const handleSubtract = () => setCounter(counter - 1)
+  const handleReset = () => setCounter(value)
 
   return (
     <Fragment>
       <h1>CounterApp</h1>
       <h2> { counter } </h2>
+      <button onClick={ handleSubtract }>-1</button>
+      <button onClick={ handleReset }>Reset</button>
       <button onClick={ handleAdd }>+1</button>
     </Fragment>
   )
